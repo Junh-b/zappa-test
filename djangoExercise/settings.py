49e7bf96eb2be9779aa65f71fb707e7aeafd5774
Baek Junha <jooha208@gmail.com>
2021-05-08 00:12:42 +0900

    add api gateway to allowed host

--- djangoExercise/settings.py
+++ djangoExercise/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     '.amazonaws.com',
-    '.junhabaek.net'
+    '.junhabaek.net',
+    os.environ['API_GATEWAY_URL']
 ]
 
 # Application definition
